4d53f1d28aab9de73a9eea4a8004c0e520254938
Shahriar <shahriarh655@gmail.com>
2024-03-01 21:14:10 +0600

    Connected to mongodb atlas successfully

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 const PORT = 5000;
+
+// mongo db url
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.caldhyv.mongodb.net/shahriar's-blog?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log("MongoDb Connected Successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//   testing routes
 
 app.get("/", (req, res) => {
   res.send("Hello from server side of mern blog app");
@@ -10,3 +26,6 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Mern blog server is running on port: ${PORT}`);
 });
+
+// mern-blog-user
+// 3lsXOdWMp7RiG0g3
