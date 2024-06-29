3bdea2a39fd78ad943ba134bd3074821649e0376
Shahriar <shahriarh655@gmail.com>
2024-06-29 13:33:40 +0600

    Added admin

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKc6EnanoKKj61vCCamKeDwXelxNzUElzIWWDgf75XNEa1-uaHgiSq32hF7bp73Tq9nsY&usqp=CAU",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
