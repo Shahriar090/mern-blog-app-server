5d908ce6ebd45585707e3af0c5ce05431e84c120
Shahriar <shahriarh655@gmail.com>
2024-07-04 13:26:04 +0600

    Implemented delete comment api route

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -55,10 +55,56 @@ export const likeComment = async (req, res, next) => {
       comment.likes.splice(userIndex, 1);
     }
     await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
+// edit comment
+export const editComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, "Comment not found"));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(
+        errorHandler(403, "You are not allowed to edit this comment")
+      );
+    }
+    const editedComment = await Comment.findByIdAndUpdate(
+      req.params.commentId,
+      {
+        content: req.body.content,
+      },
+      { new: true }
+    );
     res.status(200).json({
       success: true,
-      comment,
+      editedComment,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// delete comment
+export const deleteComment = async (req, res, next) => {
+  const comment = await Comment.findById(req.params.commentId);
+  if (!comment) {
+    return next(errorHandler(404, "Comment not found"));
+  }
+  if (comment.userId !== req.user.id && !req.user.isAdmin) {
+    return next(
+      errorHandler(403, "You are not allowed to delete this comment")
+    );
+  }
+  await Comment.findByIdAndDelete(req.params.commentId);
+  res.status(200).json({
+    success: true,
+    message: "Comment Has Been Deleted",
+  });
+  try {
   } catch (error) {
     next(error);
   }
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,6 +2,8 @@ import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
 import {
   createComment,
+  deleteComment,
+  editComment,
   getPostComments,
   likeComment,
 } from "../controllers/comment.controller.js";
@@ -9,5 +11,7 @@ const commentRoute = express.Router();
 commentRoute.post("/create", verifyToken, createComment);
 commentRoute.get("/getPostComments/:postId", getPostComments);
 commentRoute.put("/likeComment/:commentId", verifyToken, likeComment);
+commentRoute.put("/editComment/:commentId", verifyToken, editComment);
+commentRoute.put("/deleteComment/:commentId", verifyToken, deleteComment);
 
 export default commentRoute;
