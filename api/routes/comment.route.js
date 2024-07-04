d69491dde45fe2ed3617343696dc6fc0cb430879
Shahriar <shahriarh655@gmail.com>
2024-07-04 12:39:37 +0600

    Implemented comment and like api route

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,7 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  getPostComments,
+  likeComment,
+} from "../controllers/comment.controller.js";
 const commentRoute = express.Router();
 commentRoute.post("/create", verifyToken, createComment);
+commentRoute.get("/getPostComments/:postId", getPostComments);
+commentRoute.put("/likeComment/:commentId", verifyToken, likeComment);
 
 export default commentRoute;
